b8585e32cbd1c50f281e8bcb1c4d6e7663b91f53
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-12-24 12:24:19 +0530

    features and info

--- front_end/src/components/featuredInfo/featuredInfo.jsx
+++ front_end/src/components/featuredInfo/featuredInfo.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { ArrowDownwardSharp } from "@mui/icons-material";
 
 import "./featuredInfo.css";
 
 const featuredInfo = () => {
   return (
     <div className="featured">
-      <div className=""></div>
+      <div className="featuredItem">
+        <span className="featuredTitle">Revenue</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">10,000 LKR</span>
+          <span className="featuredMoneyRate">
+            -11.4 <ArrowDownwardSharp />
+          </span>
+        </div>
+        <span className="featuredSub">Compared to last month</span>
+      </div>
     </div>
   );
 };
