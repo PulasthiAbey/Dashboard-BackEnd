2225fd8268464d18381617d9648d0f97b48db2bc
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-12-24 20:00:26 +0530

    redux store configured

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { setChartData } from "./redux/actions/chartActions";
+
 import SideBar from "./components/sidebar/Sidebar";
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
@@ -6,6 +10,59 @@ import Home from "./pages/home/Home";
 import "./App.css";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const updateReduxStore = () => {
+    const data = [
+      {
+        name: "June",
+        cost: 40000,
+        sales: 24000,
+        Gross_Profit: -16000,
+      },
+      {
+        name: "July",
+        cost: 30000,
+        sales: 13980,
+        Gross_Profit: -16020,
+      },
+      {
+        name: "August",
+        cost: 20000,
+        sales: 98000,
+        Gross_Profit: 78000,
+      },
+      {
+        name: "September",
+        cost: 27800,
+        sales: 39008,
+        Gross_Profit: 11208,
+      },
+      {
+        name: "October",
+        cost: 18900,
+        sales: 48000,
+        Gross_Profit: 29100,
+      },
+      {
+        name: "November",
+        cost: 23900,
+        sales: 38000,
+        Gross_Profit: 14100,
+      },
+      {
+        name: "December",
+        cost: 34900,
+        sales: 43000,
+        Gross_Profit: 8100,
+      },
+    ];
+    dispatch(setChartData(data));
+  };
+
+  useEffect(() => {
+    updateReduxStore();
+  }, []);
+
   return (
     <div className="App">
       <TopBar />
--- front_end/src/components/chart/Chart.jsx
+++ front_end/src/components/chart/Chart.jsx
@@ -9,54 +9,57 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
+import { useDispatch, useSelector } from "react-redux";
 
 import "./chart.css";
 
 const Chart = () => {
-  const data = [
-    {
-      name: "June",
-      cost: 40000,
-      sales: 24000,
-      Gross_Profit: -16000,
-    },
-    {
-      name: "July",
-      cost: 30000,
-      sales: 13980,
-      Gross_Profit: -16020,
-    },
-    {
-      name: "August",
-      cost: 20000,
-      sales: 98000,
-      Gross_Profit: 78000,
-    },
-    {
-      name: "September",
-      cost: 27800,
-      sales: 39008,
-      Gross_Profit: 11208,
-    },
-    {
-      name: "October",
-      cost: 18900,
-      sales: 48000,
-      Gross_Profit: 29100,
-    },
-    {
-      name: "November",
-      cost: 23900,
-      sales: 38000,
-      Gross_Profit: 14100,
-    },
-    {
-      name: "December",
-      cost: 34900,
-      sales: 43000,
-      Gross_Profit: 8100,
-    },
-  ];
+  let data = useSelector((state) => state.dummyChart.chartData);
+  console.log(data);
+  // const data = [
+  //   {
+  //     name: "June",
+  //     cost: 40000,
+  //     sales: 24000,
+  //     Gross_Profit: -16000,
+  //   },
+  //   {
+  //     name: "July",
+  //     cost: 30000,
+  //     sales: 13980,
+  //     Gross_Profit: -16020,
+  //   },
+  //   {
+  //     name: "August",
+  //     cost: 20000,
+  //     sales: 98000,
+  //     Gross_Profit: 78000,
+  //   },
+  //   {
+  //     name: "September",
+  //     cost: 27800,
+  //     sales: 39008,
+  //     Gross_Profit: 11208,
+  //   },
+  //   {
+  //     name: "October",
+  //     cost: 18900,
+  //     sales: 48000,
+  //     Gross_Profit: 29100,
+  //   },
+  //   {
+  //     name: "November",
+  //     cost: 23900,
+  //     sales: 38000,
+  //     Gross_Profit: 14100,
+  //   },
+  //   {
+  //     name: "December",
+  //     cost: 34900,
+  //     sales: 43000,
+  //     Gross_Profit: 8100,
+  //   },
+  // ];
   return (
     <div className="chart">
       <h4 className="chartTitle">Sales & Analytics</h4>
