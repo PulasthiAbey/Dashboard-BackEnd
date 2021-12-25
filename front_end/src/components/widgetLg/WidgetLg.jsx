a757e9831356e6163df5d6aa12de89a66bd7e5d9
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-12-25 12:49:18 +0530

    Home page completed

--- front_end/src/components/widgetLg/WidgetLg.jsx
+++ front_end/src/components/widgetLg/WidgetLg.jsx
@@ -19,7 +19,7 @@ const WidgetLg = () => {
         <tr className="widgetLgTr">
           <td className="widgetLgUser">
             <img
-              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              src={require("../assets/images/man.jpg")}
               alt=""
               className="widgetLgImg"
             />
