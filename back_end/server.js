74a0e6ac62c3ffefda69bc9308a68058e0276a8f
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-12-28 00:57:32 +0530

    servr initiated

--- back_end/server.js
+++ back_end/server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const PORT = process.env.PORT || 3001;
-const app = require("./app");
+const app = require("./api/app/app");
 const server = http.createServer(app);
 
 server.listen(PORT);
