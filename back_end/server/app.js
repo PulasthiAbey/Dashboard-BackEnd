63020b152f1f74b5e9e7b7fff9398c7c7800a2a0
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-12-27 23:40:29 +0530

    server configurations

--- back_end/server/app.js
+++ back_end/server/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, "../front_end/build")));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
