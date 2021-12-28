375176edc840dd47c611c0bbd4817e4d740864d7
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-12-28 22:19:37 +0530

    routes/products

--- back_end/api/routes/shop/products.js
+++ back_end/api/routes/shop/products.js
@@ -29,16 +29,25 @@ router.get("/get/:name", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const product = new Products({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.params.name,
-    type: req.params.type,
-    description: req.params.description,
-    price: req.params.price,
-    quantity: req.params.quantity,
-  });
+    const product = new Products({
+      _id: new mongoose.Types.ObjectId(),
+      name: req.params.name,
+      type: req.params.type,
+      description: req.params.description,
+      price: req.params.price,
+      quantity: req.params.quantity,
+    });
+
+//   const productTemp = new Products({
+//     _id: new mongoose.Types.ObjectId(),
+//     name: "Test",
+//     type: "Test Type",
+//     description: "This is a test",
+//     price: 100,
+//     quantity: 250,
+//   });
   try {
-    const result = await product.save();
+    const result = await productTemp.save();
     res.json(result);
   } catch (err) {
     res.json({ message: "Error While saving the post" });
--- back_end/app.js
+++ back_end/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const morgan = require("morgan");
 const cors = require("cors");
 const bodyParser = require("express").json;
 require("dotenv").config();
@@ -9,7 +8,6 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Middleware configuration
-app.use(morgan("dev"));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
