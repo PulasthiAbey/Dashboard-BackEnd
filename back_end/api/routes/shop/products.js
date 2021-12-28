44abc6ae847f0133bdc0afaf71d47b5781eef8cb
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-12-28 21:26:17 +0530

    routes/products

--- back_end/api/routes/shop/products.js
+++ back_end/api/routes/shop/products.js
@@ -42,87 +42,8 @@ router.post("/", async (req, res) => {
     res.json(result);
   } catch (err) {
     res.json({ message: "Error While saving the post" });
-    res.status(500).send(err);
+    console.log(err);
   }
 });
-
-
-// router.post("/", (req, res, next) => {
-//   const product = new Products({
-//     _id: new mongoose.Types.ObjectId(),
-//     name: req.params.name,
-//     type: req.params.type,
-//     description: req.params.description,
-//     price: req.params.price,
-//     quantity: req.params.quantity,
-//   });
-
-//   product
-//     .save()
-//     .then((result) => {
-//       console.log("New Product Created Successfully" + result);
-//       res.status(200).json(result);
-//     })
-//     .catch((error) => {
-//       console.log("Error occurred while creating a new product");
-//       console.log(error);
-//       res.status(500).json(error);
-//     });
-
-//   res.status(201).json({
-//     message: "Product created successfully",
-//     createdProduct: product,
-//   });
-// });
-
-// router.get("/:productId", (req, res, next) => {
-//   const productId = req.params.productId;
-//   Products.findById(productId)
-//     .exec()
-//     .then((doc) => {
-//       console.log(doc);
-//       res.status(200).json(doc);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json({ error: err });
-//     });
-// });
-
-// router.patch("/:productId", (req, res, next) => {
-//   res.status(200).json({
-//     message: "updated Product",
-//   });
-// });
-
-// router.delete("/:productId", (req, res, next) => {
-//   res.status(200).json({
-//     message: "deleted Product",
-//   });
-// });
 
 module.exports = router;
-
-/*
-
-
-// add a new Guide to the collection
-router.post("/", async (req, res) => {
-  const post = new Suggestions({
-    locationID: crypto.randomBytes(16).toString("hex"),
-    name: req.body.name,
-    description: req.body.description,
-  });
-  try {
-    // console.log("Testing the try in the save post item thing", req.body);
-    const savedPost = await post.save();
-    console.log("Object after the save Post call", savedPost);
-    res.json(savedPost);
-  } catch (err) {
-    console.log("err" + err);
-    res.json({ message: "Error While saving the post" });
-    res.status(500).send(err);
-  }
-});
-
-module.exports = router;*/
--- back_end/app.js
+++ back_end/app.js
@@ -33,19 +33,6 @@ mongoose
     console.log(err);
   });
 
-// Headers
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, GET, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
 
 // Root Route
 app.get("/", (req, res) => {
@@ -58,22 +45,6 @@ const orderRoute = require("./api/routes/shop/order");
 // Routes
 app.use("/shop/products", productsRoutes);
 app.use("/shop/order", orderRoute);
-
-// Error handling
-app.use((req, res, next) => {
-  const error = new Error("Not Found");
-  error.status = 404;
-  next(error);
-});
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message,
-    },
-  });
-});
 
 //listening from the server_error
 app.listen(PORT, () => {
